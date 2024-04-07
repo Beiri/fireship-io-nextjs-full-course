7612ce45be35320a1f478a073e7c21edd6fdf528
Beiri <beiri.al@outlook.com>
2024-04-07 12:21:55 -0300

    Loading UI

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,11 @@ export default async function Home() {
 
   return (
     <div>
-
+      <h1>Welcome to NextSpace!</h1>
+      <p>
+        A next-gen social media app to connect with frens inspired by MySpace
+      </p>
+      <p>To get started, sign up for an account</p>
     </div>
   );
 }
