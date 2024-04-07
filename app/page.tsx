88d5316bb3bd7746f2542dcf5e833b10cd75a592
Beiri <beiri.al@outlook.com>
2024-04-07 00:35:22 -0300

    Protect routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
-import styles from "./page.module.css";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    redirect('/api/auth/signin')
+  }
+
   return (
     <main>
 
