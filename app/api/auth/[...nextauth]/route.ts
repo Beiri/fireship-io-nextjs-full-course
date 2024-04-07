77c0a0b62411c763ccf680b0011027d2f6d6f573
Beiri <beiri.al@outlook.com>
2024-04-07 01:07:24 -0300

    Auth data store

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,11 @@
+import { prisma } from "@/lib/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth, { NextAuthOptions } from "next-auth"
+import { Adapter } from "next-auth/adapters"
 import Github from "next-auth/providers/github"
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     Github({
       clientId: process.env.GITHUB_ID!,
